35e160c8026ca1734b2bdd06d1d54e3df33fe4ca
王治尧 <1217167460@qq.com>
2019-11-25 10:28:56 +0800

    修改配置名

--- src/service/axios.js
+++ src/service/axios.js
@@ -3,9 +3,9 @@ import axios from "axios"
 export default class HttpRequest {
     // 禁止警告报错
     /* eslint-disable */
-
+    queue = {}
     constructor() {
-        this.queue = {}
+        // this.queue = {}
     }
 
     getInsideconfig() {
@@ -30,17 +30,17 @@ export default class HttpRequest {
         }, error => {
             return Promise.reject(error);
         }),
-        // 响应拦截器
-        instance.interceptors.response.use(res => {
-            // 添加全局控制
-            delete this.queue[url];
-            const { data, status } = res;
-            return { data, status };
-            // return res;
-        }, error => {
-            delete this.queue[url];
-            return Promise.reject(error);
-        })
+            // 响应拦截器
+            instance.interceptors.response.use(res => {
+                // 添加全局控制
+                delete this.queue[url];
+                const { data, status } = res;
+                return { data, status };
+                // return res;
+            }, error => {
+                delete this.queue[url];
+                return Promise.reject(error);
+            })
     }
 
     request(options) {
